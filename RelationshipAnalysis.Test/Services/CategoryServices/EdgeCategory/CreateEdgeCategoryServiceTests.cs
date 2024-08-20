4836d84aaa69e3f595dd035cecb1b704eeb47e24
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2024-08-20 11:30:22 +0330

    Pass CreateEdgeCategory Tests.

--- RelationshipAnalysis.Test/Services/CategoryServices/EdgeCategory/CreateEdgeCategoryServiceTests.cs
+++ RelationshipAnalysis.Test/Services/CategoryServices/EdgeCategory/CreateEdgeCategoryServiceTests.cs
@@ -1,4 +1,7 @@
-﻿using RelationshipAnalysis.Dto.Category;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RelationshipAnalysis.Context;
+using RelationshipAnalysis.Dto.Category;
 using RelationshipAnalysis.Enums;
 using RelationshipAnalysis.Services.CategoryServices.EdgeCategory;
 
@@ -7,10 +10,34 @@ namespace RelationshipAnalysis.Test.Services.CategoryServices.EdgeCategory;
 public class CreateEdgeCategoryServiceTests
 {
     private readonly CreateEdgeCategoryService _sut;
+    private readonly IServiceProvider _serviceProvider;
 
     public CreateEdgeCategoryServiceTests()
     {
-        _sut = new();
+        var serviceCollection = new ServiceCollection();
+
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        serviceCollection.AddScoped(_ => new ApplicationDbContext(options));
+
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+
+        _sut = new(_serviceProvider);
+
+        SeedDatabase();
+    }
+
+    private void SeedDatabase()
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.EdgeCategories.Add(new Models.Graph.EdgeCategory()
+        {
+            EdgeCategoryName = "ExistName"
+        });
+        context.SaveChanges();
     }
 
     [Fact]
@@ -26,7 +53,7 @@ public class CreateEdgeCategoryServiceTests
         Assert.Equal(StatusCodeType.BadRequest, result.StatusCode);
         Assert.Equal(Resources.NullDtoErrorMessage, result.Data.Message);
     }
-    
+
     [Fact]
     public async Task CreateEdgeCategory_ShouldReturnBadRequest_WhenCategoryNameIsNotUnique()
     {
@@ -43,7 +70,7 @@ public class CreateEdgeCategoryServiceTests
         Assert.Equal(StatusCodeType.BadRequest, result.StatusCode);
         Assert.Equal(Resources.NotUniqueCategoryNameErrorMessage, result.Data.Message);
     }
-    
+
     [Fact]
     public async Task CreateEdgeCategory_ShouldReturnSuccess_WhenDtoIsOk()
     {
@@ -52,12 +79,16 @@ public class CreateEdgeCategoryServiceTests
         {
             EdgeCategoryName = "NotExistName"
         };
+        using var scope = _serviceProvider.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         // Act
         var result = await _sut.CreateEdgeCategory(dto);
+        var categoty = context.EdgeCategories.SingleOrDefault(c => c.EdgeCategoryName == dto.EdgeCategoryName);
 
         // Assert
         Assert.Equal(StatusCodeType.Success, result.StatusCode);
         Assert.Equal(Resources.SuccessfulCreateCategory, result.Data.Message);
+        Assert.NotNull(categoty);
     }
 }
